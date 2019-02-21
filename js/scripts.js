84ea584ed79226956fd6a040b13c4007bcbe39de
Nicholas Haugh <haughlp@gmail.com>
2019-02-21 13:14:54 -0800

    Created a repository array of pokemon objects
    Each object contains name, type, and height key-value pairs

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
 alert('Hello world');
+var repository = [
+  {
+    name: 'Bulbasaur',
+    type: ['grass', 'poison'],
+    height: 0.7
+  },
+  {
+    name: 'Charmander',
+    type: ['fire'],
+    height: 0.6
+  },
+  {
+    name: 'Squirtle',
+    type: ['water'],
+    height: 0.5
+  },
+  {
+    name: 'Mewtwo',
+    type: ['psychic'],
+    height: 2
+  },
+  {
+    name: 'Pikachu',
+    type: ['electric'],
+    height: 0.4
+  }
+];
