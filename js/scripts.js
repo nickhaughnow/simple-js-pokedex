a82d175212181a09ee7a8ec7331b0a66089a1e05
Nicholas Haugh <haughlp@gmail.com>
2019-02-21 13:19:19 -0800

    Added a for loop that displays all of the pokemon objects
    Also added a conditional that displays text for the tallest pokemon.
    
    And created another conditional that displays each pokemon's type, along with the CSS class that changes the color of each pokemon type accordingly.

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,41 @@ var repository = [
     height: 0.4
   }
 ];
+
+for (i = 0; i < repository.length; i++) {
+  document.write(repository[i].name + ' (height: ' + repository[i].height + 'm)');
+
+  if (repository[i].height > 1.5) {
+    document.write('<strong> - Wow, that\'s big!</strong> ');
+  }
+
+  if (repository[i].type[0] === 'grass') {
+    document.write(' Type: ' + '<span class="green">grass</span><br>');
+  } else if (repository[i].type[0] === 'poison') {
+    document.write(' Type: ' + '<span class="purple">poison</span><br>');
+  } else if (repository[i].type[0] === 'fire') {
+    document.write(' Type: ' + '<span class="red">fire</span><br>');
+  } else if (repository[i].type[0] === 'water') {
+    document.write(' Type: ' + '<span class="blue">water</span><br>');
+  } else if (repository[i].type[0] === 'electric') {
+    document.write(' Type: ' + '<span class="yellow">electric</span><br>');
+  } else if (repository[i].type[0] === 'psychic') {
+    document.write(' Type: ' + '<span class="violet">psychic</span><br>');
+  }
+}
+
+// for (i = 0; i < repository.length; i++) {
+//     if (repository[i].type[0] === 'grass') {
+//       document.write(' Type: ' + '<span class="green">grass</span><br>');
+//     } else if (repository[i].type[0] === 'poison') {
+//       document.write(' Type: ' + '<span class="purple">poison</span><br>');
+//     } else if (repository[i].type[0] === 'fire') {
+//       document.write(' Type: ' + '<span class="red">fire</span><br>');
+//     } else if (repository[i].type[0] === 'water') {
+//       document.write(' Type: ' + '<span class="blue">water</span><br>');
+//     } else if (repository[i].type[0] === 'electric') {
+//       document.write(' Type: ' + '<span class="yellow">electric</span><br>');
+//     } else if (repository[i].type[0] === 'psychic') {
+//       document.write(' Type: ' + '<span class="violet">psychic</span><br>');
+//   }
+// }
